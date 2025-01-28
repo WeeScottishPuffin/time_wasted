8f46776385bc939307d75a365a88dc67f750bb0a
Naut van der Winden <supposedbrit@gmail.com>
2025-01-28 09:03:45 +0100

    FIX: Stripethrough not working

--- title.js
+++ title.js
@@ -5,13 +5,13 @@ title_text = title_obj.innerHTML;
 
 function start_title()
 {
-    for (let i = 0; i < title_text.length + 1; i++) {
+    for (let i = 1; i < title_text.length + 1; i++) {
         setTimeout(()=>{
             console.log(i)
-            title_obj.innerHTML = title_text.slice(-title_text.length + i,-1);
-            inner_obj.innerHTML = title_text.slice(0,i)
+            title_obj.innerHTML = (i == title_text.length) ? "" : title_text.slice(-title_text.length + i);  //2nd span "start"
+            inner_obj.innerHTML = title_text.slice(0,i)                         //1st span
         },50*i)
-        setTimeout(()=>{sub_obj.style.display = "block";},500);
+        setTimeout(()=>{sub_obj.style.display = "block";},50*(title_text.length+5));
     }
 }
 
